0e429e4645407eef1fed78683e48aa6af7bc0a21
yasoobkhalid1 <yasoobkhalid1@berkeley.edu>
2019-09-21 17:55:56 -0700

    started dealing with corrupted images

--- face_detector/face_detector.py
+++ face_detector/face_detector.py
@@ -9,18 +9,22 @@
 
 import sys
 import dlib
+import time
 from pathlib import Path
 
 detector = dlib.get_frontal_face_detector()
 win = dlib.image_window() #to show the detected face in a GUI screen
 
 #list of image filepaths
-pathlist = Path('sample_photos').glob('*.jpg*')
+pathlist = Path("sample_photos").glob('*.jpg*')
 
 for path in pathlist:
     path_str = str(path) #since path is an object
     print(path_str)
-    img = dlib.load_rgb_image(path_str)
+    try: 
+        img = dlib.load_rgb_image(path_str)
+    except RuntimeError:
+        
     detections = detector(img, 1) #upsampled the image to allow greater detection
     print("Num of faces : {}".format(len(detections)))
     for i, d in enumerate(detections):
@@ -29,7 +33,7 @@ for path in pathlist:
     win.clear_overlay()
     win.set_image(img)
     win.add_overlay(detections)
-    dlib.hit_enter_to_continue()
+    time.sleep(1)
 
 
 
