d5fa0446512210119182123e2f0e4b0e9f876876
yasoobkhalid1 <yasoobkhalid1@berkeley.edu>
2019-11-08 00:26:50 -0800

    iterating over multiple batch sizes to find optimum

--- photo_ethnicity_classifier/vgg_classifier.py
+++ photo_ethnicity_classifier/vgg_classifier.py
@@ -4,49 +4,53 @@ from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras import optimizers
 
-img_folder = 'total_sample'
+img_folder = 'small_sample'
+batches = {16:1024, 32:512, 64:256, 128:128}
 
-train_datagen = ImageDataGenerator(
-    width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True,
-    rotation_range=20, brightness_range=[0.5, 1.5], zoom_range=0.2,
-    shear_range=0.2, validation_split=0.2
-)
+for batch in batches:
 
-train_generator = train_datagen.flow_from_directory(
-    img_folder, color_mode='rgb', target_size=(150, 150), batch_size=128, subset='training',
-    shuffle=True
-)
+    train_datagen = ImageDataGenerator(
+        width_shift_range=0.2, height_shift_range=0.2, horizontal_flip=True,
+        rotation_range=20, brightness_range=[0.5, 1.5], zoom_range=0.2,
+        shear_range=0.2, validation_split=0.2
+    )
 
-test_generator = train_datagen.flow_from_directory(
-    img_folder, color_mode='rgb', target_size=(150, 150), batch_size=128, subset='validation',
-    shuffle=True
-)
+    train_generator = train_datagen.flow_from_directory(
+        img_folder, color_mode='rgb', target_size=(150, 150), batch_size=batch, subset='training',
+        shuffle=True
+    )
 
-# load model without classifier layers
-model = VGG16(include_top=False, input_shape=(150, 150, 3))
+    test_generator = train_datagen.flow_from_directory(
+        img_folder, color_mode='rgb', target_size=(150, 150), batch_size=batch, subset='validation',
+        shuffle=True
+    )
 
-# for layer in model.layers:
-# 	layer.trainable = False
+    # load model without classifier layers
+    model = VGG16(include_top=False, input_shape=(150, 150, 3))
 
-# add new classifier layers
-flat1 = Flatten()(model.output)
-class1 = Dense(1024, activation='relu')(flat1)
-class1 = Dense(512, activation='relu')(flat1)
-output = Dense(4, activation='softmax')(class1)
-# define new model
-model = Model(inputs=model.inputs, outputs=output)
+    # for layer in model.layers:
+    # 	layer.trainable = False
 
-# custom optimizer
-sgd = optimizers.SGD(lr=0.00001, decay=1e-6, momentum=0.9, nesterov=True)
+    # add new classifier layers
+    flat1 = Flatten()(model.output)
+    class1 = Dense(1024, activation='relu')(flat1)
+    class1 = Dense(512, activation='relu')(flat1)
+    output = Dense(4, activation='softmax')(class1)
+    # define new model
+    model = Model(inputs=model.inputs, outputs=output)
 
-#compile model using accuracy to measure model performance
-model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+    # custom optimizer
+    sgd = optimizers.SGD(lr=0.00001, decay=1e-6, momentum=0.9, nesterov=True)
 
-epoch_steps = 128
+    # compile model using accuracy to measure model performance
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
-#train the model
-model.fit_generator(train_generator, steps_per_epoch=epoch_steps,
-                    validation_data=test_generator, validation_steps=epoch_steps,
-                    epochs=1)
+    epoch_steps = batches[batch]
 
-model.save('vgg.h5')
+    # train the model
+    model.fit_generator(train_generator, steps_per_epoch=epoch_steps,
+                        validation_data=test_generator, validation_steps=epoch_steps,
+                        epochs=1)
+
+    model.save('vgg_batch_{}.h5'.format(batch))
+
